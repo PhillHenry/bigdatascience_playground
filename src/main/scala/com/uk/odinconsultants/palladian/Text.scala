7abdbba2f60b622e929d009a893fe75fc1fe62f8
PhillHenry <phillhenry@gmail.com>
2018-04-16 15:30:00 +0100

    Working ngrams.

--- src/main/scala/com/uk/odinconsultants/palladian/Text.scala
+++ src/main/scala/com/uk/odinconsultants/palladian/Text.scala
@@ -4,4 +4,6 @@ object Text {
 
   def alphaNumeric(x: String): String = x.replaceAll("""[^a-zA-Z0-9]""", "")
 
+  def toNgrams(x: String, min: Int, max: Int): Seq[String] = (min to max).flatMap(i => x.sliding(i))
+
 }
--- src/test/scala/com/uk/odinconsultants/palladian/TextSpec.scala
+++ src/test/scala/com/uk/odinconsultants/palladian/TextSpec.scala
@@ -2,15 +2,35 @@ package com.uk.odinconsultants.palladian
 
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.annotation.tailrec
+
 class TextSpec extends WordSpec with Matchers {
 
   import Text._
+
+  @tailrec
+  final def factorial(x: Int, acc: Int): Int = {
+    if (x <= 1) {
+      acc
+    } else {
+      factorial(x - 1, acc * x)
+    }
+  }
 
   "Text" should {
     "be alpha numeric only" in {
       val actual = "12phill"
       alphaNumeric(s"!%^${actual}*()") shouldBe actual
     }
+    "turned into n-grams" in {
+      val text    = "0123456789"
+      val length  = text.length
+      val max     = 10
+      val min     = 3
+      val ngrams  = toNgrams(text, min, max)
+      ngrams should have length (min to max).map(x => max - x + 1).sum
+    }
   }
+
 
 }
